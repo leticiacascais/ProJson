e252da7521f8604619357544070335cff454d6ea
Letícia Gonçalves Cascais <leticia.cascais@gmail.com>
2026-04-27 19:22:19 +0100

    feat: implement JsonReference and add unit tests

---
+++ src/test/kotlin/projson/JsonReferenceTest.kt
@@ -0,0 +1,14 @@
+package projson
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class JsonReferenceTest {
+
+    @Test
+    fun testReferenceToString() {
+        val ref = JsonReference("123")
+
+        assertEquals("{\"" + "$" + "ref\": \"123\"}", ref.toString())
+    }
+}
