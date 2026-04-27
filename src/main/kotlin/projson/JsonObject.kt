6de32455d5cac33deb9b6a4182d18d74cfc2282f
Letícia Gonçalves Cascais <leticia.cascais@gmail.com>
2026-04-27 18:32:07 +0100

    feat: implement JsonObject and JsonArray

--- src/main/kotlin/projson/JsonObject.kt
+++ src/main/kotlin/projson/JsonObject.kt
@@ -1,4 +1,23 @@
 package projson
 
-class JsonObject {
+class JsonObject : JsonValue {
+
+    private val properties = mutableMapOf<String, JsonValue>()
+
+    fun setProperty(key: String, value: JsonValue) {
+        properties[key] = value
+    }
+
+    fun getProperty(key: String): JsonValue? {
+        return properties[key]
+    }
+
+    fun removeProperty(key: String) {
+        properties.remove(key)
+    }
+
+    override fun toString(): String {
+        return properties.entries.joinToString(prefix = "{", postfix = "}") { (key, value) -> "\"$key\": $value"
+        }
+    }
 }
